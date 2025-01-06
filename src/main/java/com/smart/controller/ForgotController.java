d18dae4b64bd36659a71373827baf5404d423768
Akash Singh <akash214singh@gmail.com>
2025-01-06 20:25:58 +0530

    Added GIT Url for testing purpose in Forget Controller

--- src/main/java/com/smart/controller/ForgotController.java
+++ src/main/java/com/smart/controller/ForgotController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -154,6 +155,13 @@ public class ForgotController {
 			this.userRepository.save(user);
 			
 			return "redirect:/signin?change=Password changed Successfully";
+		}
+		
+		//adding GetMapping URL for Git branching...
+		
+		@GetMapping("/git-url")
+		public String getStringResponse() {
+			return "GIT is Awesome";
 		}
 	
 }
